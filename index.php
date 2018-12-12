72cf2292f2f6940698d19f4a45968c9bbc8311ca
salle11 <salle11@etudiant2.ifts.univ-reims.fr>
2018-12-12 15:30:54 +0100

    Captcha amelioré

--- index.php
+++ index.php
@@ -4,25 +4,31 @@
 <meta charset=utf-8" />
 </head>
 <body>
+<center><h1>TP Captcha</h1></center>
+<p>Saisissez le code ci-dessous</p>
 <?php
 session_start();
 /* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
 * AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
+//variable qui contient ce qu'il va afficher
+$affichage = "<img src='script-captchas.php' alt='captchas' />";
 
-echo "<img src='script-captchas.php' alt='captchas' />";
-
-
-
+//si on clique sur valider
 if (isset($_POST['send'])) {
 	$answer = $_POST['captcha'];
+	//si l'input est vide
 	if (empty($answer)) {
-		echo "Le code de sécurité doit être impérativement saisi !";
+		echo "<p style='color: red;'>Le code de sécurité doit être impérativement saisi !</p>";
+	//si l'input est correct
 	}elseif ($answer == $_SESSION['code']) {
-		echo "Code valide";
+		//$affichage prendra cette valeur ce qui effacera l'image du captcha
+		$affichage = "<p style='color: green;' >Code valide</p>";
+	//si l'input est incorrecte
 	}else{
-		echo "Code invalide";
+		echo "<p style='color: red;'>Code invalide</p>";
 	}
 }
+echo $affichage;
 ?>
 <form method="POST" action="#">
 	<input type="text" name="captcha">
@@ -30,3 +36,7 @@ if (isset($_POST['send'])) {
 </form>
 </body>
 </html>
+<?php
+
+
+?>
